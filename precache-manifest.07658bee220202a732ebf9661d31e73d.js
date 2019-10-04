6fe76023a4a0fad0f5aeca04e6008bd4eec74a59
Thanh Nguyen <thanh@inspectorio.com>
2019-10-04 10:39:00 +0700

    #1

---
+++ precache-manifest.07658bee220202a732ebf9661d31e73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5d6d6f7639f9b6ac7878319b8312ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84eebbf6584131219d9a",
+    "url": "/static/css/main.06825bc0.chunk.css"
+  },
+  {
+    "revision": "81eed601abca489398cb",
+    "url": "/static/js/2.87633736.chunk.js"
+  },
+  {
+    "revision": "84eebbf6584131219d9a",
+    "url": "/static/js/main.953e9a5a.chunk.js"
+  },
+  {
+    "revision": "126075b94937cb5b2cb7",
+    "url": "/static/js/runtime-main.86411dc8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
